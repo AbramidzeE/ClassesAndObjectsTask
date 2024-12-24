2e0e1bb69aa4a2b163c61b05c48010717266f355
Jeko <Jeko@DESKTOP-37UQCU1>
2024-12-24 11:51:56 +0400

    Add project files.

---
+++ ClassesAndObjectsTask/Model/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjectsTask.Model
+{
+    internal class Employee
+    {
+        private string surname { get; set; }
+        private int age { get; set; }
+
+        public Employee()
+        {
+                
+        }
+
+        public Employee(string surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        private string ReturnAge()
+        {
+            return age.ToString();
+        }
+
+        public void UpdateSurname(string newSurname)
+        {
+            surname = newSurname;
+        }
+
+        public string ReturnEmplyeeInfo()
+        {
+            return (surname, age).ToString();
+        }
+    }
+}
